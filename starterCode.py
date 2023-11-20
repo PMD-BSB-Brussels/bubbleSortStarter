1a662723efbe3314a7bb19596bc9e0612d0e6804
PMD-BSB-Brussels <148069059+PMD-BSB-Brussels@users.noreply.github.com>
2023-11-20 12:50:22 +0100

    Update starterCode.py

--- starterCode.py
+++ starterCode.py
@@ -1,2 +1,15 @@
+# The following code contains a number of logical errors and one run time error, please fix them.
 
-numbers = []
+numbers = [4,2,9,7,0,1,3,10]
+
+sorted = True
+while not sorted:
+  sorted = False
+  for i in range(1,len(numbers):
+    if numbers[i] > numbers[i+1]:
+      temp = numbers[i]
+      numbers[i] = temp
+      numbers[i+1] = numbers[i]
+      sorted = True
+
+print(numbers)
