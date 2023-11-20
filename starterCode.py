285aa3523e40155246da82640b28efafda1511ff
PMD-BSB-Brussels <148069059+PMD-BSB-Brussels@users.noreply.github.com>
2023-11-20 12:59:35 +0100

    Update starterCode.py

--- starterCode.py
+++ starterCode.py
@@ -2,14 +2,16 @@
 
 numbers = [4,2,9,7,0,1,3,10]
 
-sorted = True
-while not sorted:
-  sorted = False
-  for i in range(1,len(numbers):
-    if numbers[i] > numbers[i+1]:
-      temp = numbers[i]
-      numbers[i] = temp
-      numbers[i+1] = numbers[i]
-      sorted = True
+def bubbleSort(numbers):
+  sorted = True
+  while not sorted:
+    sorted = False
+    for i in range(1,len(numbers):
+      if numbers[i] > numbers[i+1]:
+        temp = numbers[i]
+        numbers[i] = temp
+        numbers[i+1] = numbers[i]
+        sorted = True
+  return numbers
 
-print(numbers)
+print(bubbleSort(numbers))
